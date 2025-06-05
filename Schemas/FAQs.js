9619870007f19db7ebe93371f501a02a169e29d6
Paoox <pao.arreola.g@gmail.com>
2025-06-04 18:47:16 -0600

    eliminando bd

--- Schemas/FAQs.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const faqSchema = new mongoose.Schema({
-  pregunta: { type: String, required: true },
-  respuesta: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Faq', faqSchema);
